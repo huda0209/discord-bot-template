ca8ab4d885afa2b0b6093aac7021c07746ec482e
huda0209 <huda030209@gmail.com>
2020-09-27 23:12:40 +0900

    [add]コンソールに色付きでログを出力する機能を追加

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ main.js :MAIN  'MAIN CODE'　<= this
  
 ran by node.js
 
-2020-9-26
+2020-9-27
 
 */
 
@@ -23,13 +23,14 @@ const msgEvent = require('./src/msgEvent.js')
 const option = {ws: {intents: discord.Intents.ALL}};
 const json = JSON.parse(fs.readFileSync('./config/setting.json','utf8'));
 const client = new discord.Client(option);
+const color = require('./src/color');
 const PREFIX = json.PREFIX;
 const letter = [[":zero:","0⃣"],[":one:","1⃣"],[":two:","2⃣"],[":three:","3⃣"],[":four:","4⃣"],[":five:","5⃣"],[":six:","6⃣"],[":seven:","7⃣"],[":eight:","8⃣"],[":nine:","9⃣"],[":keycap_ten:","🔟"],[":regional_indicator_a:","🇦"],[":regional_indicator_b:","🇧"],[":regional_indicator_c:","🇨"],[":regional_indicator_d:","🇩"],[":regional_indicator_e:","🇪"],[":regional_indicator_f:","🇫"],[":regional_indicator_g:","🇬"],[":regional_indicator_h:","🇭"],[":regional_indicator_i:","🇮"],[":regional_indicator_j:","🇯"],[":regional_indicator_k:","🇰"],[":regional_indicator_l:","🇱"],[":regional_indicator_m:","🇲"],[":regional_indicator_n:","🇳"],[":regional_indicator_o:","🇴"],[":regional_indicator_p:","🇵"],[":regional_indicator_q:","🇶"],[":regional_indicator_r:","🇷"],[":regional_indicator_s:","🇸"],[":regional_indicator_t:","🇹"],[":regional_indicator_u:","🇺"],[":regional_indicator_v:","🇻"],[":regional_indicator_w:","🇼"],[":regional_indicator_x:","🇽"],[":regional_indicator_y:","🇾"],[":regional_indicator_z:","🇿"]]
 
 
 //start the bot
 client.on("ready", message => {
-  console.log(`bot is ready! ver. ${json.VERSION} \nlogin: ${client.user.tag}`);
+  console.log(`${color.header('info')} bot is ready! ver. ${json.VERSION} \n       login: ${color.chcol('cyan')}${client.user.tag}${color.reset()}\n`);
   client.user.setActivity(`${PREFIX}helpでヘルプを表示 ver. ${json.VERSION}`, { type: 'PLAYING' });
 });
 
@@ -56,13 +57,13 @@ client.on("message", async message => {
   try{
     DATA = JSON.parse(fs.readFileSync(`./config/guild/${message.guild.id}.json`,'utf8'));
   }catch (error){
-    console.log(error);
+    console.log(`${color.header('error')}fail to load guild file \n${error}\n`);
     return};
   if (message.content.startsWith(PREFIX)){
     const [command, ...args] = message.content.slice(PREFIX.length).split(' ');
 
     if(command === "stop" &&(message.author.id === message.guild.ownerID)){
-      console.log(`server was stoped by ${message.author.tag}`);
+      console.log(`${color.header('info')}server was stoped by ${message.author.tag}\n`);
       await message.delete();
       client.destroy();
       process.exit(0)};
@@ -92,7 +93,7 @@ client.on("messageReactionAdd", async(messageReaction ,user) =>{
 
 
 if(json.MAIN_TOKEN == undefined || json.MAIN_TOKEN == ""){
-  console.log("please set setting.json : MAIN_TOKEN");
+  console.log(`${color.header('error')}please set setting.json : ${color.chcol('cyan')}MAIN_TOKEN${color.reset()}`);
   process.exit(0);
 }
 let token;
@@ -103,13 +104,13 @@ if(process.argv.length>=3){
       break;
     case "div" :
       if(json.DIV_TOKEN == undefined || json.DIV_TOKEN == ""){
-        console.log("please set setting.json : DIV_TOKEN");
+        console.log(`${color.header('error')}please set setting.json : ${color.chcol('cyan')}DIV_TOKEN${color.reset()}`);
         process.exit(0)};
       token = json.DIV_TOKEN;
       json.VERSION = `dev(${json.VERSION})`;
       break;
     default :
-      console.log(`\nUnknown command. \nUsage \n node main.js main : use main token \n node main.js div : use divelopment token`);
+      console.log(`${color.header('error')}Unknown command. \n${color.chcol('cyan')}Usage${color.reset()} \n node main.js main : use main token \n node main.js div : use divelopment token`);
       process.exit(0);
   };
 }else token = json.MAIN_TOKEN;
