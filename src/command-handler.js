ef321e5d254e4a152ad468aed948d47f027b2c40
huda0209 <huda030209@gmail.com>
2020-10-11 23:16:20 +0900

    [add]コマンドハンドラを追加

--- src/command-handler.js
+++ src/command-handler.js
@@ -12,19 +12,15 @@ ran by node.js
 
 */
 
-/*
-const dotenv = require('dotenv').config();
-const fs = require('fs');
-const discord = require("discord.js");
-const client = new discord.Client();
-const letter = [[":zero:","0⃣"],[":one:","1⃣"],[":two:","2⃣"],[":three:","3⃣"],[":four:","4⃣"],[":five:","5⃣"],[":six:","6⃣"],[":seven:","7⃣"],[":eight:","8⃣"],[":nine:","9⃣"],[":keycap_ten:","🔟"],[":regional_indicator_a:","🇦"],[":regional_indicator_b:","🇧"],[":regional_indicator_c:","🇨"],[":regional_indicator_d:","🇩"],[":regional_indicator_e:","🇪"],[":regional_indicator_f:","🇫"],[":regional_indicator_g:","🇬"],[":regional_indicator_h:","🇭"],[":regional_indicator_i:","🇮"],[":regional_indicator_j:","🇯"],[":regional_indicator_k:","🇰"],[":regional_indicator_l:","🇱"],[":regional_indicator_m:","🇲"],[":regional_indicator_n:","🇳"],[":regional_indicator_o:","🇴"],[":regional_indicator_p:","🇵"],[":regional_indicator_q:","🇶"],[":regional_indicator_r:","🇷"],[":regional_indicator_s:","🇸"],[":regional_indicator_t:","🇹"],[":regional_indicator_u:","🇺"],[":regional_indicator_v:","🇻"],[":regional_indicator_w:","🇼"],[":regional_indicator_x:","🇽"],[":regional_indicator_y:","🇾"],[":regional_indicator_z:","🇿"]]
-*/
+const ping = require('./command/ping.js')
 
+const commandHandler = async function ([command, ...args],message,guildData,BOT_DATA,client){
 
-const msgEvent = async function ([command, ...args],message,guildData,BOT_DATA,client){
-    if (command === 'ping') {
-        message.channel.send('Pong.');
-    }
+    switch(command.toLowerCase()){
+        case "ping" :
+            ping.ping([command, ...args],message,guildData,BOT_DATA,client)
+            break;
+      }
 }
 
-exports.msgEvent = msgEvent
+exports.commandHandler = commandHandler
