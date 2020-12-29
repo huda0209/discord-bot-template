ce123fffb0de14d18420f250714f3f909e486f40
huda0209 <65341393+huda0209@users.noreply.github.com>
2020-12-29 22:49:56 +0900

    [add]COMMANDチェック機能追加 初期起動時の処理を追加 初期起動時にsetting.jsonを生成する

--- src/util/config.js
+++ src/util/config.js
@@ -3,15 +3,45 @@
 created by huda0209
 
  -config.js :module  Check config data
+ ver. 1.0.2
  
 ran by node.js
 
-2020-10-23
+2020-12-29
 
 */
 'use strict'
-
+const fs = require('fs');
 const logger = require('./logger')
+
+function getConfig(){
+    try{
+        const guildData = JSON.parse(fs.readFileSync('./config/guild/guildData.json', 'utf8'));
+        return guildData;
+    }catch(e){
+        logger.error(`Failed to load file! {cyan}guildData.json{reset} Please check config file.\nIf this code ran first time, this error can ignore.`);
+    };
+}
+
+function getBotData(){
+    try{
+        const BOT_DATA = JSON.parse(fs.readFileSync('./config/setting.json', 'utf8'));
+        check(BOT_DATA);
+        return BOT_DATA;
+    }catch(e){
+        logger.error(`Failed to load file! {cyan}setting.json{reset} Please check config file.`);
+        const DATA = {
+            "NAME" : "",
+            "MAIN_TOKEN" : "",
+            "DIV_TOKEN" : "",
+            "VERSION" : "0.0.0",
+            "PREFIX" : "/",
+            "COMMAND" : ""
+        };
+        fs.writeFileSync(`./config/setting.json`, JSON.stringify(DATA, null, '\t'),'utf8');
+        process.exit(0);
+    };
+}
 
 function check(config){
     if(config.MAIN_TOKEN === null || config.MAIN_TOKEN === undefined || config.MAIN_TOKEN == ""){
@@ -26,6 +56,10 @@ function check(config){
         logger.error(`please set setting.json : {cyan}VERSION`);
         process.exit(0);
     };
+    if(config.COMMAND === null || config.COMMAND === undefined || config.COMMAND == ""){
+        logger.error(`please set setting.json : {cyan}COMMAND`);
+        process.exit(0);
+    };
 }
 
 function divCheck(config){
@@ -35,5 +69,6 @@ function divCheck(config){
     };
 }
 
-exports.check = check;
+exports.getBotData = getBotData;
+exports.getConfig = getConfig;
 exports.divCheck = divCheck;
