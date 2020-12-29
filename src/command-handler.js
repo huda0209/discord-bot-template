3f45ea154820c0945eb0c8c2db37a3a6cdc3603c
huda0209 <65341393+huda0209@users.noreply.github.com>
2020-12-29 22:47:15 +0900

    [add]admin用のコマンドハンドラー追加 stopコマンド adminコマンド

--- src/command-handler.js
+++ src/command-handler.js
@@ -1,26 +1,56 @@
 /*
 
 created by huda0209
-discord-bot v2 for discord bot 
+discord-bot for discord bot 
 
 main.js :MAIN  'MAIN CODE'
- -msgEvent.js :module  'liten some event and sort the task'  <= this
+ -command-handler.js :module  <= this
+ -admin.js :module
+
  
 ran by node.js
 
-2020-10-11
+2020-12-29
 
 */
 
-const ping = require('./command/ping.js')
+const ping = require('./command/ping.js');
+const logger = require('./util/logger');
+const admin = require('./command/admin/admin.js');
 
-const commandHandler = async function ([command, ...args],message,guildData,BOT_DATA,client){
+
+async function commandHandler([command, ...args],message,guildData,BOT_DATA,client){
 
     switch(command.toLowerCase()){
+        case BOT_DATA.COMMAND :
+            if(!(message.author.id == message.guild.ownerID || guildData.Admin.indexOf(message.author.id)>-1)) break;
+            AdminCommandHandler([command, ...args],message,guildData,BOT_DATA,client);
+            break;
+
         case "ping" :
             ping.ping([command, ...args],message,guildData,BOT_DATA,client)
             break;
-      }
+      };
+}
+
+
+async function AdminCommandHandler([command, ...args],message,guildData,BOT_DATA,client){
+
+    switch(args[0].toLowerCase()){
+        case "admin" :
+            admin.adminManager([command, ...args],message,guildData,BOT_DATA,client);
+            break;
+
+        case "stop" :
+            logger.info(`server was stoped by {cyan}${message.author.tag}`);
+            await message.delete();
+            client.destroy();
+            process.exit(0);
+        
+        default:
+            message.reply(`Unknown command.`);
+            break;
+      };
 }
 
 exports.commandHandler = commandHandler
