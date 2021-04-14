549aadc94398555172b4d87f75cccfe9986362c4
huda0209 <huda030209@gmail.com>
2021-04-14 23:03:14 +0900

    [add]バージョンチェック機能を追加

--- src/util/color.js
+++ src/util/color.js
@@ -1,26 +1,26 @@
 /*
 
 created by huda0209
-discord-bot for discord bot 
 
-main.js :MAIN  'MAIN CODE'
- -msgEvent.js :CLASS  'liten some event and sort the task'
- -color.js :module  <= this
+ -color.js :module
+  ver. 2.0.0
  
 ran by node.js
 
-2020-10-4
+2021-4-14
 
 */
+const version = "2.0.0";
+
 
 const color = {
-    'black': '\u001b[30m',
-    'red': '\u001b[31m',
-    'green': '\u001b[32m',
-    'yellow': '\u001b[33m',
-    'magenta' :  '\u001b[34m',
-    'cyan': '\u001b[36m',
-    'white': '\u001b[37m'
+   'black': '\u001b[30m',
+   'red': '\u001b[31m',
+   'green': '\u001b[32m',
+   'yellow': '\u001b[33m',
+   'magenta' :  '\u001b[34m',
+   'cyan': '\u001b[36m',
+   'white': '\u001b[37m'
 }
 
 const bgcolor = {
@@ -45,3 +45,33 @@ exports.chcol = color;
 exports.bgcolor = bgcolor;
 exports.reset = reset;
 exports.header = header;
+
+
+//verison checker
+(function(){
+   const request = require("request");
+   const logger = require("./logger");
+
+   let OriginalVersion;
+
+   const options = {
+      url : "https://raw.githubusercontent.com/huda0209/Versions/main/UtilsVersion.json",
+      method : "GET"
+   };
+
+   request(options, (error, response, body)=>{
+      if(error){
+         logger.error("{red}failed to run https request.");
+         console.log(error);
+      };
+
+      try{
+         OriginalVersion = JSON.parse(body).color;
+      }catch(e){
+         logger.error("{red}failed to parse text to json.");
+         console.log(e);
+      };
+
+      if(OriginalVersion != version) logger.warn(`{cyan}"color.js"{reset} has an {green}update{reset}. {red}${version}{reset}(now)=>{green}${OriginalVersion}{reset}(new)`);
+   });
+}());
